9c4d61c19a5979919a01b6e5eab8dd785ae41d15
Rahul Patra <Rahul.patra@techangouts.com>
2020-06-17 16:46:53 +0530

    Initial Commit

---
+++ InstantRecepie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstantRecepie
+//
+//  Created by MAC on 17/06/20.
+//  Copyright © 2020 Techangouts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
